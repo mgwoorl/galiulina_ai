6b819811966f5184e7fec558578b6e3986679143
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-02-26 03:46:59 +0800

    Update main.py

--- knn_ocr/main.py
+++ knn_ocr/main.py
@@ -1,1 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from skimage.measure import label, regionprops
+from pathlib import Path
 
+train_data_dir = "task/train/"
+test_data_dir = "task/"
